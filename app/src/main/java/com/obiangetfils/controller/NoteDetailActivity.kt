af3e53869e2046221cf9ad7850a8cd7b3853e85d
Axel ESSONO <naxelcdric@gmail>
2020-07-16 01:27:58 +0200

    Created NoteDetailActivity.kt

---
+++ app/src/main/java/com/obiangetfils/controller/NoteDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.obiangetfils.controller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.obiangetfils.notepad.R
+
+class NoteDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note_detail)
+    }
+}
