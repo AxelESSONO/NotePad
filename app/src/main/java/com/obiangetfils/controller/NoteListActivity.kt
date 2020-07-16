187393c9ac828f1ea544a0160f9b1ccf8997ad07
Axel ESSONO <naxelcdric@gmail>
2020-07-16 13:09:53 +0200

    Add new note

--- app/src/main/java/com/obiangetfils/controller/NoteListActivity.kt
+++ app/src/main/java/com/obiangetfils/controller/NoteListActivity.kt
@@ -2,21 +2,19 @@ package com.obiangetfils.controller
 
 import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import android.widget.Toolbar
-import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.obiangetfils.adapter.NoteAdapter
 import com.obiangetfils.notepad.R
 import com.obiangetfils.notepad.model.Note
 
 class NoteListActivity : AppCompatActivity(), View.OnClickListener {
 
-    lateinit var notes : MutableList<Note>
+    lateinit var notes: MutableList<Note>
     lateinit var adapter: NoteAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,6 +23,8 @@ class NoteListActivity : AppCompatActivity(), View.OnClickListener {
 
         val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
+
+        findViewById<FloatingActionButton>(R.id.create_note_fab).setOnClickListener(this)
 
         //Load List
         notes = mutableListOf<Note>()
@@ -42,10 +42,10 @@ class NoteListActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
 
-        if (resultCode != Activity.RESULT_OK || data == null){
+        if (resultCode != Activity.RESULT_OK || data == null) {
             return
         }
-        when(requestCode){
+        when (requestCode) {
             NoteDetailActivity.REQUEST_EDIT_NOTE -> processEditNoteResult(data)
         }
 
@@ -59,22 +59,35 @@ class NoteListActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     private fun saveNote(note: Note, noteIndex: Int) {
-        notes[noteIndex] = note
+
+        if (noteIndex < 0) {
+            notes.add(0, note)
+        } else {
+            notes[noteIndex] = note
+        }
         adapter.notifyDataSetChanged()
     }
 
-    fun showNoteDetail(noteIndex : Int){
-        val note = notes[noteIndex]
+    fun showNoteDetail(noteIndex: Int) {
+        val note = if (noteIndex < 0) Note() else notes[noteIndex]
         val intent = Intent(this, NoteDetailActivity::class.java)
         intent.putExtra(NoteDetailActivity.EXTRA_NOTE, note)
         intent.putExtra(NoteDetailActivity.EXTRA_NOTE_INDEX, noteIndex)
         startActivityForResult(intent, NoteDetailActivity.REQUEST_EDIT_NOTE)
     }
 
+    private fun createNewNote() {
+        showNoteDetail(-1)
+    }
+
     override fun onClick(view: View) {
 
-        if (view.tag != null){
+        if (view.tag != null) {
             showNoteDetail(view.tag as Int)
+        } else {
+            when (view.id) {
+                R.id.create_note_fab -> createNewNote()
+            }
         }
     }
 
